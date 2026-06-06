adee3d31d60b1119bcc83b5177bde13693eb6c10
Matheus030205 <matheustessmann03@gmail.com>
2026-06-05 22:13:15 -0300

    STRUCTS,FALTA FAZER DO HASH

--- minirede.cpp
+++ minirede.cpp
@@ -1,5 +1,5 @@
 #include "minirede.h"
-## teste 
+
 void inicializarMiniRede(MiniRede& rede) {
     // TODO
 }
--- minirede.h
+++ minirede.h
@@ -7,6 +7,59 @@ const int TAM_USERNAME = 50;
 const int TAM_NOME = 100;
 const int TAM_TEXTO = 280;
 const int TAM_COMANDO = 30;
+const int TAM_HASH = 1001;
+
+
+
+struct Usuario
+{
+    int id;
+    char username [TAM_USERNAME];
+    char nome [TAM_NOME];
+
+    NoListaUsuario* seguidos;//NO DO INICIO DA LISTA DE PESSOAS SEGUIDAS
+    NoLista_de_Post* posts_do_usuario;//No do inicio da lista de publicacoes
+    // No do inicio dos post do usuario
+
+    //no do inicio e fim da fila duplamente encadeada das notificacoes
+
+};
+
+struct Publicacao
+{
+    int id_da_publicacao;
+    int autor_id;
+    int timestamp;
+    char texto_da_publicacao[TAM_TEXTO];
+    int qtd_likes;
+
+    NoListaUsuario* curtidas;//LISTA DE IDS QUEM CURTIU O POST
+    //Ponteiro inicio da lista de usuario q curtiram a publicacao
+
+};
+
+struct NoListaUsuario
+{
+    int id;
+    NoListaUsuario *prox;
+};
+
+struct NoArvoreUsuarios
+{
+    Usuario* usuario;
+    NoArvoreUsuarios* esq;
+    NoArvoreUsuarios* dir;
+};
+struct NoLista_de_Post
+{
+    NoLista_de_Post*prox;
+    Publicacao *publicacao_atual;
+};
+
+struct NoHashUsuario
+{
+    /* data */
+};
 
 // TODO: definir as structs principais do trabalho.
 //
@@ -22,6 +75,11 @@ const int TAM_COMANDO = 30;
 // Os campos de cada struct fazem parte do projeto dos alunos.
 
 struct MiniRede {
+
+    NoArvoreUsuarios* raiz_id; //USO No comando add_user,find_user e list_user
+    NoHashUsuario* tabela_usernames[TAM_HASH]; // USO PARA INDEXAR OS USUARIOS SOBRE A STRING DO USERNAME
+    NoLista_de_Post* todos_os_posts;
+
     // TODO: declarar aqui os ponteiros/estruturas principais da rede.
     //
     // Exemplos de responsabilidades:
